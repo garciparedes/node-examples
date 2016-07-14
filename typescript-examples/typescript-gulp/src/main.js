8a5e3a345357cd432cd21390eeece93f50d7d911
garciparedes <garciparedes@gmail.com>
2016-07-14 17:48:46 +0200

    Added TypeScript gulp tutorial

---
+++ typescript-examples/typescript-gulp/src/main.js
@@ -0,0 +1,7 @@
+"use strict";
+var greet_1 = require("./greet");
+function showHello(divName, name) {
+    var elt = document.getElementById(divName);
+    elt.innerText = greet_1.sayHello(name);
+}
+showHello("greeting", "TypeScript");
