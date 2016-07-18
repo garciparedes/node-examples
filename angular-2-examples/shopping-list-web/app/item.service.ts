12049ef9debd06a8efa949441f91c5ccdbfb3279
garciparedes <garciparedes@gmail.com>
2016-07-18 16:50:13 +0200

    working on auth

--- angular-2-examples/shopping-list-web/app/item.service.ts
+++ angular-2-examples/shopping-list-web/app/item.service.ts
@@ -14,8 +14,26 @@ export class ItemService {
     constructor(private http: Http) { }
 
 
+
+    createAuthorizationHeader(headers: Headers) {
+        headers.append('Authorization', 'Basic ' + btoa('garciparedes:1234'));
+    }
+
+
     getItems(): Promise<Item[]> {
-        return this.http.get(this.itemsUrl)
+        //let headers = new Headers();
+        //this.createAuthorizationHeader(headers);
+
+        let auth = 'Basic ' + btoa('garciparedes:1234');
+
+        let headers = new Headers( {
+            'Content-Type': 'application/json',
+            'Authorization': auth,
+            'Access-Control-Allow-Origin': '*',
+        });
+
+
+        return this.http.get(this.itemsUrl, { headers: headers })
             .toPromise()
             .then(response => response.json());
     }
--- angular-2-examples/shopping-list-web/app/item.ts
+++ angular-2-examples/shopping-list-web/app/item.ts
@@ -2,5 +2,4 @@ export class Item {
     id: number;
     name: string;
     done: boolean;
-    username: string;
 }
